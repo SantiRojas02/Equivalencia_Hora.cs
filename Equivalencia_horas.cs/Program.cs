b781761b0d68068891fbbf4981ca03b052f8ce1b
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-08 22:17:53 -0300

    Equivalencia_Horas.cs

---
+++ Equivalencia_horas.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Equivalencia_horas.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int dias, segundos, minutos, hora;
+            Console.Write("Ingrese hora: ");
+            hora =  Convert.ToInt32(Console.ReadLine());
+    
+
+            dias = 24 / hora;
+            segundos = 3600 / hora;
+            minutos = 60 / hora;
+
+            Console.WriteLine("Equivalente en dias: " + dias);
+            Console.WriteLine("Equivalente en segundos: " + segundos);
+            Console.WriteLine("Equivalente en minutos: " + minutos);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
